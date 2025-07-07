921feaaad0be11ff7de61a9f19361517410d2940
cuilinhao <15021538370@163.com>
2025-07-07 18:30:55 +0800

    添加数据统计

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 import { Toaster } from "@/components/ui/toaster"
 
@@ -15,6 +16,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-97PFDD4QSG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-97PFDD4QSG');
+          `}
+        </Script>
+      </head>
       <body>
         {children}
         <Toaster />
